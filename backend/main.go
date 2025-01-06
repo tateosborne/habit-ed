268e0c36745b626104a7814cb8cfc853c3620211
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-01-05 22:43:08 -0500

    database parameters

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"fmt"
 	_ "database/sql"
+	"fmt"
 
-	_ "github.com/lib/pq"  // the _ tells go we need this package even though we never rf it
-						   // this is the database driver
+	_ "github.com/lib/pq" // the _ tells go we need this package even though we never rf it
 )
 
 const (
-    host = "localhost"
-    port = 5432
-    user = "tateposborne"
-    dbname = "habit-ed"
+	host   = "localhost"
+	port   = 5432
+	user   = "tateposborne"
+	dbname = "habit-ed"
 )
 
 func main() {
