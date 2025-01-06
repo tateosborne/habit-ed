736889cc186ad41ff0bd7275df8fc2b894d7dfbc
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-01-05 19:35:16 -0500

    created go mod and setting up psql integration

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"database/sql"
+	
+	_ "github.com/lib/pq"
+)
+
+const (
+    host = "localhost"
+    port = 5432
+    user = "tateposborne"
+    dbname = "habit-ed"
 )
 
 func main() {
 	fmt.Println("hello")
-	
 }
